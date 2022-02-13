2e4459c457ed3499d0c62c30ac2a191333f3a818
axeljarletoft <60604922+axeljarletoft@users.noreply.github.com>
2022-02-13 14:10:17 +0100

    Källkod + Exekverbar version
    Den exekverbara filen tar emot en .txt-fil som argument.

---
+++ converter.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace converter {
+
+    class newSystem {
+
+        static void Main(string[] args) {
+            
+            //file
+            string filename = args[0];
+
+            // create & begin XML file
+            var sts = new XmlWriterSettings() {
+                Indent = true,
+            };
+
+            using var writer = XmlWriter.Create("testFile.xml", sts);
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("people");
+
+            // declare needed arr&var 
+            string[] tags;
+            string prevoiusLetter = "X";
+
+            // read file line by line
+            foreach (string line in System.IO.File.ReadLines(filename)) {
+                
+                // first line -  ' P|Carl Gustaf|Bernadotte '
+                string[] separatedLine = line.Split("|"); // array is now {P, Carl Gustaf, Bernadotte}
+
+                // initialiaze array with x in case of error in txt file
+                tags = new string[] {"x"};
+
+                switch(separatedLine[0]) {
+
+                    case "P":
+                        if (prevoiusLetter != "P")
+                            tags = new string[] {"person", "fistname", "lastname"};
+                    break;
+                    
+                    case "T":
+                        tags = new string[] {"phone", "mobile", "landline"};
+                    break;
+
+                    case "A":
+                        tags = new string[] {"address", "street", "city", "zipcode"};
+                    break;
+
+                    case "F":
+                        if(prevoiusLetter != "F" || prevoiusLetter != "P")
+                            tags = new string[] {"family", "name", "born"};
+                    break;
+                }
+
+                // write to xml file
+                if(tags[0] != "x")
+                    writer.WriteStartElement(tags[0]);
+                    for(int i = 1; i < separatedLine.Length; i++) {
+                        writer.WriteStartElement(tags[i]);
+                        writer.WriteString(separatedLine[i]);
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+            }
+
+            // close XML file
+            writer.WriteEndDocument(); // end of people
+            Console.WriteLine("XML document created");
+        }
+    }
+}
